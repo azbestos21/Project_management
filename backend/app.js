b8644c897a77f0486809ca29e83b23d5446f15e7
azbestos21 <142704258+azbestos21@users.noreply.github.com>
2024-01-18 15:04:22 +0530

    first commit

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
