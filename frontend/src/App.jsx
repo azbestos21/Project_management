7fdfa942b3a11a9c72c27382d72840e3e330f853
Rahul TN <devangarahultn@gmail.com>
2024-02-17 12:41:53 +0530

    Latest

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,14 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Dash from "./Dashboard/Dash.jsx";
 import Loginpg from "./StudAuth/Loginpg.jsx";
+import { PrivateRoute } from "./StudAuth/privateroute.jsx";
 function App() {
   return (
     <Routes>
+      <Route path="/" element={<PrivateRoute />}>
+        <Route path="/dashboard" element={<Dash />} />
+      </Route>
       <Route path="/login" element={<Loginpg />} />
-      <Route path="/dashboard" element={<Dash />} />
     </Routes>
   );
 }
