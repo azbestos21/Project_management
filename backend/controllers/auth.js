99ff94ec2a2f46c26745921e056e1917793fa467
Rahul TN <devangarahultn@gmail.com>
2024-02-22 11:56:02 +0530

    changes

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 const mentorTableModule = require("../models/mentor");
 const projectTableModule = require("../models/project");
 const studentTableModule = require("../models/student");
-const multer = require("multer")
+const multer = require("multer");
 const fs = require("fs");
 const nodemailer = require("nodemailer");
 require("dotenv").config();
@@ -381,106 +381,64 @@ exports.studentteam = (req, res) => {
   });
 };
 
-<<<<<<< HEAD
-//FRONTEND PENDING
 exports.uploadphase = (req, res) => {
   const usn = req.user;
-  const { file } = req.body;
+
   console.log(usn);
 
   const getPID = "SELECT P_ID from student S where S.USN=?";
   connection.query(getPID, [usn], (err, data) => {
     if (data) {
       const PID = data[0].P_ID;
-      const updateProject = `UPDATE project SET Phase_Status='uploaded', File_Path = '${file}' WHERE project.Project_ID=${PID}`;
-
-      connection.query(updateProject, (err, data) => {
-        if (err) {
-          console.log("Cant update into project");
-          res.status(500).json({ msg: "Internal server error" });
-        } else {
-          console.log("successful updation into project");
-          res.status(200).json({ msg: "updated record in project" });
-        }
-      });
-    } else {
-      console.log("Internal server error");
-      res.status(500).json({ msg: "Internal server error" });
-    }
-  });
-};
-=======
-
-exports.uploadphase = (req,res) =>{
-  const usn = req.user;
-  
-  console.log(usn);
-
-  
-
-  const getPID="SELECT P_ID from student S where S.USN=?";
-  connection.query(getPID,[usn],(err,data)=>{
-
-    if(data){
-      const PID=data[0].P_ID;
       const storage = multer.diskStorage({
         destination: function (req, file, cb) {
           const uploadDir = "C:/uploadsRishi/";
-    
+
           // Check if the directory exists
           if (!fs.existsSync(uploadDir)) {
             // If not, create the directory
             fs.mkdirSync(uploadDir);
           }
-    
+
           cb(null, uploadDir);
         },
         filename: function (req, file, cb) {
           cb(null, file.originalname); // Specify how the uploaded files will be named
         },
       });
-    
+
       const upload = multer({ storage: storage });
-    
+
       upload.single("document")(req, res, async function (err) {
         if (err) {
           return res.status(500).send("invalid document");
-    }
-    
-      const uploadedFile = await req.file;
-      console.log(uploadedFile);
-    
-      const path=uploadedFile.destination + uploadedFile.filename;
-      console.log(path);
-      const updateProjectFile=`UPDATE project SET Phase_Status='uploaded', File_Path='${path}' WHERE project.Project_ID=${PID}`;
-
-      connection.query(updateProjectFile,(err,data)=>{
-        if(err){
-          console.log("Cant update file into project");
-          
-          return res.status(500).json({msg:"Internal server error",err})
         }
-        else{
-          console.log("successful updation of file into project");
-          return res.status(200).json({msg:"updated record in project"})
-        }
-      })
-    });
 
-      
-      
+        const uploadedFile = await req.file;
+        console.log(uploadedFile);
 
+        const path = uploadedFile.destination + uploadedFile.filename;
+        console.log(path);
+        const updateProjectFile = `UPDATE project SET Phase_Status='uploaded', File_Path='${path}' WHERE project.Project_ID=${PID}`;
 
-    }
-    else{
+        connection.query(updateProjectFile, (err, data) => {
+          if (err) {
+            console.log("Cant update file into project");
+
+            return res.status(500).json({ msg: "Internal server error", err });
+          } else {
+            console.log("successful updation of file into project");
+            return res.status(200).json({ msg: "updated record in project" });
+          }
+        });
+      });
+    } else {
       console.log("Internal server error");
-      return res.status(500).json({msg:"Internal server error"})   
-     }
-    
-  })
-}
+      return res.status(500).json({ msg: "Internal server error" });
+    }
+  });
+};
 
->>>>>>> 63a8affd497b05308cf77d6bd7864b89b2d7d58f
 const sendemail = async (email) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
@@ -551,14 +509,8 @@ const sendemail = async (email) => {
   }
 };
 
-<<<<<<< HEAD
 exports.acceptProject = (req, res) => {
   const { pid } = req.body;
-=======
-exports.acceptProject=(req,res)=>{  
-
-  const {pid} =req.body;
->>>>>>> 63a8affd497b05308cf77d6bd7864b89b2d7d58f
   console.log(pid);
   const acceptedQuery = `
     UPDATE project 
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,6 @@ const express = require("express");
 const authController = require("../controllers/auth");
 const mid = require("../middleware/auth");
 const router = express.Router();
-<<<<<<< HEAD
 router.post("/register", authController.mentorregister);
 router.post("/login", authController.mentorlogin);
 router.post("/studentregister", authController.studentregister);
@@ -11,7 +10,7 @@ router.post(
   "/projectregister",
   mid.authenticationMiddleware,
   authController.projectregister
-); //FRONTEND PENDING
+);
 router.get(
   "/viewgroups",
   mid.mentorauthenticationMiddleware,
@@ -31,26 +30,12 @@ router.post(
   "/studentupload",
   mid.authenticationMiddleware,
   authController.uploadphase
-); //FRONTEND PENDING
+);
 router.get(
   "/studentteam",
   mid.authenticationMiddleware,
   authController.studentteam
-); //FRONTEND PENDING
+);
 router.post("/acceptproject", authController.acceptProject);
 router.post("/rejectproject", authController.rejectProject);
-=======
-router.post('/register', authController.mentorregister);
-router.post('/login', authController.mentorlogin);
-router.post('/studentregister', authController.studentregister);
-router.post('/studentlogin', authController.studentlogin);
-router.post('/projectregister', mid.authenticationMiddleware,authController.projectregister);
-router.get('/viewgroups', mid.mentorauthenticationMiddleware,authController.grouplist);
-router.get('/viewprojects', mid.mentorauthenticationMiddleware,authController.projectlist);
-router.get('/studentproject', mid.authenticationMiddleware,authController.studentproject);
-router.post('/studentupload',mid.authenticationMiddleware,authController.uploadphase)
-router.get('/studentteam',mid.authenticationMiddleware,authController.studentteam)
-router.post('/acceptproject',authController.acceptProject);
-router.post('/rejectproject',authController.rejectProject);
->>>>>>> 63a8affd497b05308cf77d6bd7864b89b2d7d58f
 module.exports = router;
