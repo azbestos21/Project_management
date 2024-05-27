6146c8346fa24fb3d9372c91bcda589a63814b77
Rahul TN <devangarahultn@gmail.com>
2024-05-27 16:47:25 +0530

    Correctios done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,18 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 
 import Loginpg from "./StudAuth/Loginpg.jsx";
-import  PrivateRoute  from "./StudAuth/privateroute.jsx";
+import PrivateRoute from "./StudAuth/PrivateRoute.jsx";
 import Login from "./MentorAuth/Login.jsx";
-import  PrivateMentRoute  from "./MentorAuth/PrivateMentRoute.jsx";
+import PrivateMentRoute from "./MentorAuth/PrivateMentRoute.jsx";
 import LogPg from "./AdminAuth/logPg.jsx";
-import   PrivateRouter  from "./AdminAuth/PrivateRoute.jsx";
+import PrivateRouter from "./AdminAuth/PrivateRoute.jsx";
 
 import MyMentees from "./MentorPages/MyMentees.jsx";
 import MProjects from "./MentorPages/MProjects.jsx";
 import Multipage from "./MentorPages/Multipage.jsx";
 import MDashboard from "./MentorPages/MDashboard.jsx";
 import Actions from "./Adminpages/CreateProject.jsx";
-import Assign  from "./Adminpages/Assign.jsx";
+import Assign from "./Adminpages/Assign.jsx";
 import SDashboard from "./StudentPages/Sdashboard.jsx";
 import MulPages from "./StudentPages/MulPages.jsx";
 import Sprojects from "./StudentPages/Sprojects.jsx";
@@ -40,19 +40,23 @@ function App() {
       <Route path="/Mlogin" element={<Login />} />
       <Route path="/Alogin" element={<LogPg />} />
       <Route path="/verify-admin" element={<VerifyAdmin />} />
-      <Route path="/" element={<LogPg />} />
+      <Route path="/" element={<PrivateMentRoute />}>
         <Route path="/" element={<Multipage />}>
-        <Route path="/Adashboard" element={<ADashboard />} />
-        <Route path="/ACalendar" element={<ACalendar />} />
-        <Route path="/Assign" element={<Assign />} />
-        <Route path="/CreateProject" element={<Actions />} />
           <Route path="/Mdashboard" element={<MDashboard />} />
           <Route path="/Mymentees" element={<MyMentees />} />
           <Route path="/Mprojects" element={<MProjects />} />
           <Route path="/MCalendar" element={<MCalendar />} />
         </Route>
+      </Route>
+      <Route path="/" element={<PrivateRoute />}>
+        <Route path="/" element={<Multipage />}>
+          <Route path="/Adashboard" element={<ADashboard />} />
+          <Route path="/ACalendar" element={<ACalendar />} />
+          <Route path="/Assign" element={<Assign />} />
+          <Route path="/CreateProject" element={<Actions />} />
+        </Route>
+      </Route>
     </Routes>
-    
   );
 }
 
