feeb149f3bd17ec186b8fde5f938c7b56048a0cc
azbestos21 <142704258+azbestos21@users.noreply.github.com>
2024-05-29 14:35:55 +0530

    remove env

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql2')
 require('dotenv').config();
 const connection = mysql.createConnection({
-    host :'localhost',
-    database : 'PMS',
-    user :'root',
-    password: process.env.DB_PASSWORD
+    host: process.env.DATABASE_HOSTNAME,
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
 });
 module.exports = connection
