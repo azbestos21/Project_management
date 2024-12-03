05fad16a1ed48302e817b0d56ca0245a0dac726b
azbestos21 <142704258+azbestos21@users.noreply.github.com>
2024-12-03 13:57:02 +0530

    vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default {
+  server: {
+    host: true, // Or use "0.0.0.0" to bind to all interfaces
+    port: 5173, // Optional: specify the port
+  },
+};
